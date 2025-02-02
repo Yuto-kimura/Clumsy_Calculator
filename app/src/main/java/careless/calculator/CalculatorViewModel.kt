22c1a5ccdbcda32cce5c3f51a428202bfe532c54
Collie <collie@CollienoMacBook-Pro.local>
2025-02-03 02:18:03 +0900

    おっとっと画面の表示

--- app/src/main/java/careless/calculator/CalculatorViewModel.kt
+++ app/src/main/java/careless/calculator/CalculatorViewModel.kt
@@ -4,43 +4,77 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import org.mozilla.javascript.Context
+import kotlin.random.Random
 
 class CalculatorViewModel : ViewModel() {
 
-    private val _equationText = MutableStateFlow("")
-    val equationText = _equationText.asStateFlow()
-
-    private val _resultText = MutableStateFlow("0")
+    private val _resultText = MutableStateFlow("")
     val resultText = _resultText.asStateFlow()
 
+    private val _isMistake = MutableStateFlow(false)
+    val isMistake = _isMistake.asStateFlow()
+
+    private val _falseResult = MutableStateFlow<String>("")
+    val falseResult = _falseResult.asStateFlow()
+
     fun onButtonClick(button: String) {
-        equationText.value.let {
+        _resultText.value.let {
             if (button == "AC") {
-                _equationText.value = ""
                 _resultText.value = "0"
+                _isMistake.value = false
                 return
             }
 
             if (button == "C") {
                 if (it.isNotEmpty()) {
-                    _equationText.value = it.substring(0, it.length - 1)
+                    _resultText.value = it.substring(0, it.length - 1)
                 }
                 return
             }
 
             if (button == "=") {
-                _equationText.value = resultText.value
+                try {
+                    val trueResult = calculateResult(resultText.value)
+                    _resultText.value = trueResult
+
+                    _isMistake.value = isMistake(trueResult)
+                    if (isMistake.value) {
+                        _falseResult.value = createFalseResult(trueResult)
+                    }
+                } catch (_: Exception) {
+                }
                 return
             }
-
-            _equationText.value = it + button
-
-            try {
-                _resultText.value = calculateResult(equationText.value)
-            } catch (_: Exception) {
-            }
-
+            _resultText.value = it + button
         }
+    }
+
+    /*
+    実際の結果に対して、1.1 ~ 1.5を掛け算する
+     */
+    fun createFalseResult(trueResult: String): String {
+        // 1.1から1.5の範囲の乱数を生成
+        val randomValue = 1.1 + (Random.nextDouble() * 0.4)
+        val trueFloatResult = trueResult.toFloat()
+        return (randomValue * trueFloatResult).toString()
+    }
+
+    /**
+     * 計算結果が3桁以上の時に、20%の確率で間違える
+     */
+    private fun isMistake(result: String): Boolean {
+        if (result.length < 2) {
+            return false
+        }
+
+        val probability = 0.2 // 20%の確率
+        val randomValue = Random.nextDouble() // 0.0 ～ 1.0 のランダムな数値を生成
+
+        if (randomValue < probability) {
+            return true
+        }
+
+        return false
     }
 
     fun calculateResult(equation: String): String {
--- app/src/main/java/careless/calculator/display/CalculatorScreen.kt
+++ app/src/main/java/careless/calculator/display/CalculatorScreen.kt
@@ -1,9 +1,12 @@
 package careless.calculator.display
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -11,13 +14,23 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -25,6 +38,8 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import careless.calculator.CalculatorViewModel
+import careless.calculator.R
+import kotlinx.coroutines.delay
 
 
 val buttonList = listOf(
@@ -38,20 +53,45 @@ val buttonList = listOf(
 @Composable
 fun CalculatorScreen(modifier: Modifier, viewModel: CalculatorViewModel) {
 
+    val isMistake = viewModel.isMistake.collectAsState().value
+    var backgroundAlpha by remember { mutableStateOf(0f) }
+    LaunchedEffect(isMistake) {
+        if (isMistake) {
+            delay(1000)
+            backgroundAlpha = 0f
+        } else {
+            backgroundAlpha = 1f
+        }
+    }
+    val animatedAlpha by animateFloatAsState(
+        targetValue = backgroundAlpha,
+        animationSpec = androidx.compose.animation.core.tween(durationMillis = 1000)
+    )
+
     Box(modifier = modifier) {
+        if (isMistake) {
+            Image(
+                painter = painterResource(id = R.drawable.musimegane), // 画像のリソースID
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .graphicsLayer(alpha = animatedAlpha),
+                contentScale = ContentScale.Crop
+            )
+        }
+
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.End
         ) {
-            Text(
-                text = viewModel.equationText.collectAsState().value,
-                style = TextStyle(
-                    fontSize = 30.sp,
-                    textAlign = TextAlign.End
-                ),
-                maxLines = 5,
-                overflow = TextOverflow.Ellipsis
-            )
+            if (isMistake) {
+                Card {
+                    CardContents(
+                        title = "おっとっと!! 正しい値は・・・",
+                        resultText = viewModel.resultText.collectAsState().value
+                    )
+                }
+            }
 
             Spacer(modifier = Modifier.weight(1f))
 
@@ -83,15 +123,23 @@ fun CalculatorScreen(modifier: Modifier, viewModel: CalculatorViewModel) {
 }
 
 @Composable
-fun CalculatorButton(buttonText: String, onClick: () -> Unit) {
+fun CalculatorButton(buttonText: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
     Box(modifier = Modifier.padding(8.dp)) {
-        FloatingActionButton(
+        Button(
             onClick = onClick,
-            modifier = Modifier.size(80.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(90.dp),
             shape = CircleShape,
-            containerColor = getColor(buttonText)
+            // TODO
+            enabled = true,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = backgroubdColor(buttonText),
+                disabledContainerColor = Color(0xFFE0E0E0)
+            )
         ) {
             Text(
+                modifier = modifier,
                 text = buttonText,
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold
@@ -100,18 +148,19 @@ fun CalculatorButton(buttonText: String, onClick: () -> Unit) {
     }
 }
 
-fun getColor(buttonText: String): Color {
-    if (buttonText == "C" || buttonText == "AC") {
-        return Color(0xFFF45336)
+@Composable
+fun backgroubdColor(buttonText: String): Color {
+    if (buttonText == "C" || buttonText == "(" || buttonText == ")") {
+        return Color(0xFFFFA726)
     }
 
-    if (buttonText == "(" || buttonText == ")") {
-        return Color.Gray
+    if (buttonText == "AC") {
+        return Color.Red
     }
 
     if (buttonText == "/" || buttonText == "*" || buttonText == "+" || buttonText == "-" || buttonText == "=") {
-        return Color(0xFFFF9800)
+        return Color(0xFF80DEEA)
     }
 
-    return Color(0xFF00C8C9)
+    return Color.LightGray
 }
